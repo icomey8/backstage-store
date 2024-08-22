4c3de4f3721d0dd3cbacc517f619ce1e716f00cc
icomey8 <i.willcomey@gmail.com>
2024-08-21 17:32:02 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Link } from 'wouter';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <nav className="p-4 bg-blue-500">
+        <ul className="flex space-x-4">
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
